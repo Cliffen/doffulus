62702f0c7393d69616a232c644a7a869318a1cd3
Benjamin Chirlin <Benjamin_Chirlin@wired.com>
2014-10-18 22:28:05 -0700

    Camera mover helper script

---
+++ Assets/Scripts/FixCam.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class FixCam : MonoBehaviour {
+
+	public List<Vector3> positions = new List<Vector3>();
+	public bool animate = false;
+
+	// Init
+	void Start () {
+		// If user provides no positions, leave camera at default
+		// else set camera position to first position in user's list
+		if ( positions.Count == 0 ) {
+			positions.Add( transform.position );
+		} else {
+			transform.position = positions[0];
+		}
+	}
+
+	// On number press, move position to given point
+	void Update () {
+		if ( Input.GetKeyDown( "1" ) && positions.Count >= 1 ) {
+			setPosition( 1 );
+		} else if ( Input.GetKeyDown( "2" ) && positions.Count >= 2 ) {
+			setPosition( 2 );
+		} else if ( Input.GetKeyDown( "3" ) && positions.Count >= 3 ) {
+			setPosition( 3 );
+		} else if ( Input.GetKeyDown( "4" ) && positions.Count >= 4 ) {
+			setPosition( 4 );
+		} else if ( Input.GetKeyDown( "5" ) && positions.Count >= 5 ) {
+			setPosition( 5 );
+		} else if ( Input.GetKeyDown( "6" ) && positions.Count >= 6 ) {
+			setPosition( 6 );
+		} else if ( Input.GetKeyDown( "7" ) && positions.Count >= 7 ) {
+			setPosition( 7 );
+		} else if ( Input.GetKeyDown( "8" ) && positions.Count >= 8 ) {
+			setPosition( 8 );
+		} else if ( Input.GetKeyDown( "9" ) && positions.Count >= 9 ) {
+			setPosition( 9 );
+		} else if ( Input.GetKeyDown( "0" ) && positions.Count >= 1 ) {
+			setPosition( 0 );
+		}
+	}
+
+	// Set camera to given position index and return true if index exists
+	public bool setPosition( int position ) {
+		if ( positions.Count > position ) {
+			transform.position = positions[ position ];
+			return true;
+		}
+		return false;
+	}
+}
--- README.md
+++ README.md
@@ -1,9 +1,8 @@
 # Doffulus
-## A Hospital Training VR Experience
+
+A Hospital Training VR Experience
 
 A basic proof of concept instructional VR experience outlining how to doff Biological Level C Personal Protective Equipment. Based on instructional video found [here](https://www.youtube.com/watch?v=ls69Tib1PjU).
-
--------------------------------
 
 ### Prospective features:
 
@@ -22,3 +21,7 @@ Mechanics:
 * Wiping surfaces
 * Moving objects
 * Advancing steps in process
+
+Dependencies:
+* Oculus Unity Integration Package
+* Leap Oculus Passthrough Pacakge
